419a44dc16700e5eb50ef74c5b9665aa8e035c30
Michel <Michel_motongi@hotmail.com>
2022-06-11 19:47:52 -0400

    Add logic for the Openweather API

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
+
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles';
+import mapStyles from './mapStyles';
 
-const Map = ({setCoordinates, setBounds, coordinates, places}) => {
+const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
     const classes = useStyles();
     const isDesktop = useMediaQuery('(min-width:600px)');
-    
+
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
-                bootstrapURLKeys={{key: 'AIzaSyCJ9J1PXdnFCkfha92pY2zRigJ8G1lkkZ8'}}
-                defaultCenter={coordinates}
-                center={coordinates}
+                bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+                defaultCenter={coords}
+                center={coords}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]} 
-                options={''}
+                options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
                 onChange={(e)=> {
-                    setCoordinates({ lat: e.center.lat, lng: e.center.lng })
+                    setCoords({ lat: e.center.lat, lng: e.center.lng })
                     setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw })
                 }}
-                onChildClick={''}   
+                onChildClick={(child) => setChildClicked(child)}   
             >
                 {places?.map((place, i) => (
                     <div
                         className={classes.markerContainer}
-                        lat={place.latitude}
-                        lng={place.longitude}
+                        lat={Number(place.latitude)}
+                        lng={Number(place.longitude)}
                         key={i}
                     >
                         {
@@ -48,6 +50,11 @@ const Map = ({setCoordinates, setBounds, coordinates, places}) => {
                                 </Paper>
                             )
                         }
+                    </div>
+                ))}
+                {weatherData?.list?.map((data, i) => (
+                    <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+                        <img src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
                     </div>
                 ))}
             </GoogleMapReact>
