8830e048d3b98632fe05e86d1c200f4d3187aaba
Michel <Michel_motongi@hotmail.com>
2022-06-10 20:07:28 -0400

    Add coordinates as well as places and useEffects to show different restaurant on the map

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import { Grid} from '@material-ui/core';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React , { useState, useEffect }from 'react';
+import { Grid, CssBaseline} from '@material-ui/core';
+
+import { getPlacesData } from './api';
 import Header from './components/Header/Header';
 import Map from './components/Map/Map';
 import List from './components/List/List';
-// import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 
-function App() {
+const App = () => {
+  const [bounds, setBounds] = useState({});
+  const [coordinates, setCoordinates] = useState({});
+  const [places, setPlaces] = useState([]);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude} }) => {
+      setCoordinates({ lat: latitude, lng: longitude});
+    })
+  }, [])
+  
+  useEffect(() => {
+    getPlacesData(bounds.sw, bounds.ne)
+      .then((data) => {
+        setPlaces(data);
+      })
+  }, [coordinates, bounds])
+  
   return (
     <>
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{width: '100%'}}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places}/>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Map />
+        <Grid item xs={12} md={8}>
+          <Map 
+            setCoordinates={setCoordinates}  
+            setBounds={setBounds}  
+            coordinates={coordinates}
+            places={places}
+          />
         </Grid>
       </Grid> 
     </>
