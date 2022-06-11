068e13054232c7ad68486769a222712603515616
Michel <Michel_motongi@hotmail.com>
2022-06-10 20:08:08 -0400

    Add props 'place' instead of hard coding

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,28 +3,18 @@ import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, 
 
 import PlaceDetails from '../PlaceDetails/PlaceDetails'
 import useStyles from './styles'
-const List = () => {
+const List = ({ places }) => {
     const classes = useStyles();
     const [type, setType] = useState('restaurants');
     const [rating, setRating] = useState('');
 
-    const places = [{ name: 'Cool Place'},
-                    { name: 'Best Beer'},
-                    { name: 'Best Steak'},
-                    { name: 'Cool Place'},
-                    { name: 'Best Beer'},
-                    { name: 'Best Steak'},
-                    { name: 'Cool Place'},
-                    { name: 'Best Beer'},
-                    { name: 'Best Steak'}
-    ];
     return (
         <div className={classes.container}>
             <Typography variant="h4"> Restaurants, Hotels & Attractions around you </Typography>
             <FormControl className={classes.formControl}>
                 <InputLabel>Type</InputLabel>
                 <Select value={type} onChange={(e) => setType(e.target.value)}>
-                    <MenuItem value="restaurant">Restaurants</MenuItem>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
                     <MenuItem value="hotels">Hotels</MenuItem>  
                     <MenuItem value="attractions">Attractions</MenuItem>      
                 </Select>
@@ -39,7 +29,7 @@ const List = () => {
                 </Select>
             </FormControl>
             <Grid container spacing={3} className={classes.list}>
-                {places ?.map((place, i) => (
+                {places?.map((place, i) => (
                     <Grid item key={i} xs={12}>
                         <PlaceDetails place={place} />
                     </Grid>
